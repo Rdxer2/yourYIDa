74dcdaab4fc1a3ba5a3d508d1b33eea590edc8aa
Rdxer <240302832@qq.com>
2015-07-19 17:11:58 +0800

    测试用的东西
    今天测试用的,,

---
+++ yida/yida/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  yida
+//
+//  Created by Xiaofeng Li  on 15/7/19.
+//  Copyright (c) 2015年 Xiaofeng Li . All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("玉玉该吃药了!\n");
+    return 0;
+}
